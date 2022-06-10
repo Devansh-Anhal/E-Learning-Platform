a016ef89956f9ef5eb31d48e401af7923d91552f
Devansh Anhal <devanhal.da@gmail.com>
2022-06-10 23:30:25 +0530

    Update settings.py
    Removed Keys

--- Elearning/settings.py
+++ Elearning/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*j0+435fseg=#2n#p1b&la+-f93-_#age)8-im8w(xo44^xin&'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
